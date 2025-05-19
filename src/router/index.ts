f2d672b8dcbc72d5c98289c14e1f08709ea9e8b9
TacioDaito <taciokikuchi@gmail.com>
2025-05-18 23:04:06 -0300

    No routes needed

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import type { RouteRecordRaw } from 'vue-router';
-import OauthRedirect from '@/components/OauthRedirect.vue';
-import OauthCallback from '@/components/OauthCallback.vue';
 
 const routes: Array<RouteRecordRaw> = [
-  { path: '/redirect', component: OauthRedirect },
-  { path: '/callback', component: OauthCallback },
 ];
 
 export const router = createRouter({
